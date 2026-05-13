5a18e283c4fe7ff3ca88425634422e7298e863c2
LusmicSam <shivampanjolia8@gmail.com>
2026-05-13 14:01:05 +0530

    chore: catch exception in vercel handler

--- GroceryDeals/api/index.php
+++ GroceryDeals/api/index.php
@@ -3,4 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>Vercel PHP Fatal Error</h1>";
+    echo "<b>Message:</b> " . $e->getMessage() . "<br><br>";
+    echo "<b>File:</b> " . $e->getFile() . " on line " . $e->getLine() . "<br><br>";
+    echo "<b>Trace:</b><br><pre>" . $e->getTraceAsString() . "</pre>";
+}
+
